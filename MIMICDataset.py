ef7c0ab35e6e49b039023800d593976adc8a9cc3
Courtney Amm <ammcourtney0@gmail.com>
2024-03-27 18:37:22 -0400

    fixed dataset and simplified training loop

--- MIMICDataset.py
+++ MIMICDataset.py
@@ -5,12 +5,13 @@ from torch.utils.data import Dataset
 class MimicDataset(Dataset):
     """Mimic Dataset - a dataset of diagnostic reports and their corresponding icd9 labels"""
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file_code, csv_file_labels):
         """
         Arguments:
             csv_file (string): Path to the csv file with data and annotations
         """
-        self.mimic_dataset = pd.read_csv(csv_file)
+        self.mimic_dataset = pd.read_csv(csv_file_code)
+        # self.full_icd_labels = pd.read_csv(csv_file_labels)
 
     def __len__(self):
         return len(self.mimic_dataset)
@@ -19,7 +20,7 @@ class MimicDataset(Dataset):
         if torch.is_tensor(idx):
             idx = idx.tolist()
         
-        text = torch.to_tensor(self.mimic_dataset.iloc[idx, 0])
-        icd9 = torch.to_tensor(self.mimic_dataset.iloc[idx, 1])
+        text = torch.to_tensor(self.mimic_dataset.iloc[idx, 4])
+        icd = torch.to_tensor(self.mimic_dataset.iloc[idx, 7])
         
-        return text, icd9
+        return text, icd
--- training_loop.py
+++ training_loop.py
@@ -4,9 +4,11 @@ from torch.optim import AdamW
 from torch.utils.data import DataLoader
 import numpy as np
 import evaluate
-from transformers import AutoTokenizer,  AutoModelForSequenceClassification, TrainingArguments, get_scheduler
+from transformers import AutoTokenizer,  OPTForSequenceClassification, TrainingArguments, get_scheduler
 from tqdm.auto import tqdm
 from MIMICDataset import MimicDataset
+from transformers import TrainingArguments, Trainer
+import pandas as pd
 
 
 def tokenize_dataset(dataset):
@@ -23,19 +25,21 @@ def calculate_accuracy(predictions, labels):
     return np.mean(predictions == labels)
 
 
-def training_loop(num_epochs):
-    dataset = MimicDataset("mimic-iv-icd9.csv")
+def training_loop(dataset_code, dataset_label, icd_code, num_epochs=50, batch_size = 8):
+    dataset = MimicDataset(dataset_code, dataset_label)
     tokenized_datasets = tokenize_dataset(dataset)
+    icd_labels = pd.read_csv(dataset_label)
+    
+    # get number of icd codes from length of the dataset_label file
+    code_count = len(icd_labels)
 
     # for testing
     small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
     small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
-    model = AutoModelForSequenceClassification.from_pretrained(
-        "facebook/opt-6.7b",
-        num_labels=5
-    )
-        
+    model = OPTForSequenceClassification.from_pretrained("ArthurZ/opt-350m-dummy-sc", num_labels=code_count, problem_type="multi_label_classification")
+    
+    # metrics are incorrect right now, i will add a custom metric for multi-label classification
     metric = evaluate.load("accuracy")
 
     def compute_metrics(eval_pred):
@@ -47,82 +51,43 @@ def training_loop(num_epochs):
     small_train_dataset = tokenized_datasets["train"].shuffle(seed=42).select(range(1000))
     small_eval_dataset = tokenized_datasets["test"].shuffle(seed=42).select(range(1000))
 
-
-    train_dataloader = DataLoader(small_train_dataset, shuffle=True, batch_size=8)
-    eval_dataloader = DataLoader(small_eval_dataset, batch_size=8)
-
-    # setup model and optimizer
-    optimizer = AdamW(model.parameters(), lr=5e-5)
-    
-    for param in model.parameters():
-        param.requires_grad = False  # freeze the model - train adapters later
-        if param.ndim == 1:
-            # cast the small parameters (e.g. layernorm) to fp32 for stability
-            param.data = param.data.to(torch.float32)
-
-    model.gradient_checkpointing_enable()  # reduce number of stored activations
-    model.enable_input_require_grads()
-
-    num_training_steps = num_epochs * len(train_dataloader)
-    lr_scheduler = get_scheduler(
-        name="linear", optimizer=optimizer, num_warmup_steps=0, num_training_steps=num_training_steps
-    )
-    
-    criterion = torch.nn.CrossEntropyLoss()
-
     # send the model to the gpu if available
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
 
-    # create a training progress bar
-    progress_bar = tqdm(range(num_training_steps))
-    
-    # evaluation lists
-    iters, train_losses, train_acc, val_acc = [], [], [], []
-    iter_num = 0
+    training_args = TrainingArguments(output_dir="test_trainer")
 
-    # training loop
-    model.train()
-    for epoch in range(num_epochs):
-        for data, labels in iter(train_dataloader):
-            # this code may be incorrect, I need to test it tomorrow            
-            # batch = {k: v.to(device) for k, v in batch.items()}
-            predictions = model(data)
-            loss = criterion(predictions, labels)
-            loss.backward()
-
-            optimizer.step()
-            lr_scheduler.step()
-            optimizer.zero_grad()
-            progress_bar.update(1)
-                
-            # test loop - check model accuracy
-            iter_num += 1
-            if iter_num % 10 == 0:
-                loss = float(loss)
-                train_losses.append(loss)
-                
-                train_acc = calculate_accuracy(predictions, labels)
-                iters.append(iter_num)
-                train_acc.append(train_acc)
-                val_acc.append(val_acc)
-                
-
-    # evaluation loop
-    # we might not need this loop, I'll test it tomorrow (3/27)
     metric = evaluate.load("accuracy")
-    model.eval()
-    for batch in eval_dataloader:
-        batch = {k: v.to(device) for k, v in batch.items()}
-        with torch.no_grad():
-            outputs = model(**batch)
 
-        logits = outputs.logits
-        predictions = torch.argmax(logits, dim=-1)
-        metric.add_batch(predictions=predictions, references=batch["labels"])
+    def compute_metrics(eval_pred):
+        logits, labels = eval_pred
+        predictions = np.argmax(logits, axis=-1)
+        return metric.compute(predictions=predictions, references=labels)
+    
+    training_args = TrainingArguments(f"opt-finetuned-{icd_code}",
+    evaluation_strategy = "epoch",
+    save_strategy = "epoch",
+    learning_rate=2e-5,
+    per_device_train_batch_size=batch_size,
+    per_device_eval_batch_size=batch_size,
+    num_train_epochs=5,
+    weight_decay=0.01,
+    load_best_model_at_end=True,
+    metric_for_best_model="accuracy")
 
-    metric.compute()
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        train_dataset=small_train_dataset,
+        eval_dataset=small_eval_dataset,
+        compute_metrics=compute_metrics,
+    )
+
+    trainer.train()
     
     
 if __name__ == "__main__":
-    training_loop(10)
+    icd_code = "icd9"
+    dataset_codes = f"data/{icd_code}_codes.csv"
+    dataset_labels = f"data/mimic-iv-{icd_code}.csv"
+    training_loop(dataset_codes, dataset_labels, icd_code)
