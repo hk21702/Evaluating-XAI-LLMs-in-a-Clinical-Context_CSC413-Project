1fb1b2f3b55e485a71e7674e8d0306f3635093cb
kuochihh <28567881+hk21702@users.noreply.github.com>
2024-04-10 15:52:50 -0400

    rename to show the data is not necessarily tokenized

--- MIMICDataset.py
+++ MIMICDataset.py
@@ -6,12 +6,12 @@ import ast
 class MimicDataset(Dataset):
     """Mimic Dataset - a dataset of diagnostic reports and their corresponding icd9 labels"""
 
-    def __init__(self, tokenized_dataset, labels, csv_file_labels):
+    def __init__(self, dataset, labels, csv_file_labels):
         """
         Arguments:
             csv_file (string): Path to the csv file with data and annotations
         """
-        self.tokenized_dataset = tokenized_dataset
+        self.dataset = dataset
         
         # create a dictionary of icd codes and their corresponding index in the list
         self.labels = labels
@@ -23,14 +23,14 @@ class MimicDataset(Dataset):
     
 
     def __len__(self):
-        return len(self.tokenized_dataset)
+        return len(self.dataset)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
           
         # get the tokenized data
-        item = self.tokenized_dataset[idx]
+        item = self.dataset[idx]
 
         icd_codes = ast.literal_eval(self.labels[idx])
         
