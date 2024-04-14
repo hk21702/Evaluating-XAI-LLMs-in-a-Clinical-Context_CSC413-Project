9736ccc09290c007512d84ddd362684b33e0600e
kuochihh <28567881+hk21702@users.noreply.github.com>
2024-04-14 19:03:55 -0400

    icd10 job script

---
+++ job_icd10.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=92G
+#SBATCH --partition csc413
+#SBATCH --gres=gpu:2
+
+export OMP_NUM_THREADS=8
+torchrun --nproc_per_node 2  manager.py train --project_name OPT-Finetuning ICD10 --train_path data/train_10.csv -val_path data/val_10.csv -test_path data/test_10.csv --code_labels data/icd10_codes.csv --checkpoint_dir opt-finetuned-icd10 --wandb_key   --fresh_start 
