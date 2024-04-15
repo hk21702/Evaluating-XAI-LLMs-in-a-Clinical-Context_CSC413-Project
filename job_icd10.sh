9392aeda2d8778a07493889df241f088f14cdb98
Courtney Amm <ammcourtney0@gmail.com>
2024-04-14 20:50:43 -0400

    fixed minor bash script errors

--- job_icd10.sh
+++ job_icd10.sh
@@ -6,4 +6,4 @@
 #SBATCH --gres=gpu:2
 
 export OMP_NUM_THREADS=8
-torchrun --nproc_per_node 2  manager.py train --project_name OPT-Finetuning ICD10 --train_path data/train_10.csv -val_path data/val_10.csv -test_path data/test_10.csv --code_labels data/icd10_codes.csv --checkpoint_dir opt-finetuned-icd10 --wandb_key   --fresh_start 
+torchrun --nproc_per_node 2  manager.py train --project_name OPT-Finetuning-ICD10 --train_path data/train_10.csv --val_path data/val_10.csv --test_path data/test_10.csv --code_labels data/icd10_codes.csv --checkpoint_dir opt-finetuned-icd10 --wandb_key 971c33204c93100c21249224f1d7e3082f3d2ad7 --fresh_start 
