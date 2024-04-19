e7ae3f8a043ca468af0b3d54965c79f775c46808
juanyiloke <lokejuanyi@gmail.com>
2024-04-19 01:19:11 -0400

    merging

--- bert_shap.py
+++ bert_shap.py
@@ -80,7 +80,6 @@ tokenizer_wrapper = TokenizerWrapper(tokenizer_bert, MAX_LEN, classes)
 dataset = load_dataset("csv", data_files=data_files)
 dataset = dataset.map(tokenizer_wrapper.tokenize_function, batched=True, num_proc=1)
 dataset = dataset.with_format("torch")
-<<<<<<< HEAD
 print("dataset loaded")
 =======
 >>>>>>> 0047235e8bd89d190923a758b15ec5db213045c0
